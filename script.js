0cf2036371c5e542a1be680af80d1a7773396c94
Andrei <126566419+Xeotheosis@users.noreply.github.com>
2023-05-26 02:48:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('resize', function() {
+    var img = document.getElementById('picture');
+  
+    if (window.innerWidth < 750) {
+      img.src = "/images/image-product-mobile.jpg";
+    } else {
+      img.src = "/images/image-product-desktop.jpg";
+    }
+  });
